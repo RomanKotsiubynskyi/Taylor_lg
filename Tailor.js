92454a3cf6b453e06d745e06bdf4cfbb5a3a5701
RomanKo <RomanKotsiubynskyi@users.noreply.github.com>
2021-06-24 19:11:23 +0300

    function name have changed to ExB

--- Tailor.js
+++ Tailor.js
@@ -1,13 +1,14 @@
-//some changes
-function Ex(x) {
+// var i = 1; var n = 0:
+function ExB(x) {
     var i;
-    //var n = 0;
+    var n = 0;
     var A = 0;
    
-    for (i = 1; i < 8; i++) {
-        A += Math.pow (x,i)*Math.pow(-1,(i-1))/i;
+    for (i = 1; i < 8; i++, n++) {
+        A += Math.pow (x,i)*Math.pow(-1,n)/i;
     }
     return A;
 };
-console.log(Ex(0.7)); //x < 1/0!
+console.log(ExB(0.7)); //x < 1/0!
 
+module.exports.TaylorA = ExB;
